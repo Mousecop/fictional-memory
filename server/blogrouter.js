d06837ba060d7ae16561a9e52577a54ddad4972e
Sim Gill <simgill89@gmail.com>
2017-02-17 12:45:06 -0800

    finished app

--- server/blogrouter.js
+++ server/blogrouter.js
@@ -41,14 +41,41 @@ router.post('/', jsonParser, function(req,res) {
 
 
 
+router.delete('/:id', function(req,res) {
+	BlogPosts.delete(req.params.id);
+	console.log(`Im deleting yo ${req.params.id}`);
+	res.status(204).end();
+});
 
 
+router.put('/:id', jsonParser, function(req,res) {
+	const requiredFields = ['title', 'content', 'author'];
+	for (let i = 0; i < requiredFields.length; i++) {
+	    const field = requiredFields[i];
+	    if (!(field in req.body)) {
+	        const message = `Missing \`${field}\` in request body`
+	        console.error(message);
+	        return res.status(400).send(message);
+	    }
 
+	}
+	if (req.params.id !== req.body.id) {
+    	const message = (
+      		`Request path id (${req.params.id}) and request body id `
+      		`(${req.body.id}) must match`);
+    	console.error(message);
+   		return res.status(400).send(message);
+  	}
+  	console.log(`Updating shopping list item \`${req.params.id}\``);
+  	const updatedItem = BlogPosts.update({
+    	id: req.params.id,
+    	title: req.body.title,
+    	content: req.body.content,
+    	author: req.body.author
+ 	});
+  res.status(204).json(updatedItem);
 
-
-router.delete('/', function(request,response) {})
-
-router.put('/', function(request,response) {})
+})
 
 
 
