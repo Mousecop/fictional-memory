e730ea42ea3931cb02daa99a451907903e6546c9
Mousecop <joaskew13@gmail.com>
2017-02-21 17:03:19 -0500

    fixed test script (again)

--- tests/test-blog-posts.js
+++ tests/test-blog-posts.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const should = chai.should()
+const should = chai.should();
 const {app, runServer, closeServer} = require('../server');
 const {BlogPosts} = require('../model');
 
