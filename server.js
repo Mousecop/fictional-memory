ee942bb2c6586281088e8dff07f2dc687410e376
Mousecop <joaskew13@gmail.com>
2017-02-21 17:07:35 -0500

    fixed pathing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const blogrouter = require('./blogrouter')
+const blogrouter = require('./server/blogrouter')
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/blog-posts', blogrouter);
